0f7a46509be537af33e419e31adaf2521f1f37b4
route1rodent <18262122+route1rodent@users.noreply.github.com>
2020-03-13 00:12:29 +0100

    add helper for updating gh-pages

--- src/tasks/build.sh
+++ src/tasks/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+rm -f static/data/pokemon-boxes.json
 ./src/tasks/install.sh
 gatsby clean
 gatsby build
---
+++ src/tasks/update-gh-pages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+if [[ ! -d "gh-pages" ]]; then
+  git clone git@github.com:capsumon/livingdex.git gh-pages
+  cd gh-pages
+  git checkout gh-pages
+  cd -
+fi
+
+mv gh-pages/.git gh-pages-git
+rm -rf gh-pages/*
+cp -$ public/ gh-pages
+mv gh-pages-git gh-pages/.git
+
+cd gh-pages
+git add -A
+git commit -m "update gh-pages"
+git log
+cd -
