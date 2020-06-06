ad49c5faf95b351258b3dfd45243b5601e46a2ff
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2020-06-06 07:34:25 +0200

    fix publish with prefixes

--- src/tasks/update-gh-pages.sh
+++ src/tasks/update-gh-pages.sh
@@ -4,20 +4,21 @@ if [[ ! -d "gh-pages" ]]; then
   git clone git@github.com:itsjavi/livingdex.git gh-pages
   cd gh-pages
   git checkout gh-pages
-  git pull --rebase
   cd -
 fi
 
+git pull --rebase || exit 1
 mv gh-pages/.git gh-pages-git
 rm -rf gh-pages/*
 
-./src/tasks/build.sh
+export PREFIX_PATHS="true"
+PREFIX_PATHS="true" ./src/tasks/build.sh
+
 cp -R public/ gh-pages
 mv gh-pages-git gh-pages/.git
 
 cd gh-pages
 git add -A
 git commit -m "update gh-pages"
-git push
 
 cd -
