7ac4322929e6be1151ff1419bcb3e24784aa1c61
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2021-03-29 08:07:34 +0200

    Updates

--- component---src-pages-404-js-3e1c9575c52f67d90232.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("7oih"),o=n("EYWl");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-3e1c9575c52f67d90232.js.map
